9bb8c96024f3682b55a2006f117358f1e6578aff
Brandon <brandon.schoelkoph@yahoo.com>
2026-05-24 20:00:30 -0700

    Enable CORS middleware to allow cross-origin requests

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 // Initialise database tables on startup
 init().catch((err) => {
   console.error('Failed to initialise database', err);
